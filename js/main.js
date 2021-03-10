e1938d4a3fa3871c67cdb3cd4e8fe2b76377cb72
Brigitta Varga <v.brigitta.zs@gmail.com>
2021-03-10 16:02:04 +0100

    styled blog page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const toggleClose = document.querySelector(".toggle-close");
 const nav = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".mobile-nav li");
 const burgerIcon = document.getElementById("burger");
+const inviteBtn = document.querySelectorAll(".invite-btn");
+const modalBg = document.querySelector(".modal-bg");
+const modalClose = document.querySelector(".modal-close");
 
  // Toggle menu function
 function toggleFunction() {
@@ -21,13 +24,10 @@ function toggleFunction() {
  toggleFunction();
 
  // Get invite modal on invite-btn click
-const modalBg = document.querySelector(".modal-bg");
-const modalClose = document.querySelector(".modal-close");
-
 function modalFunction() {
     modalBg.classList.add("modal-active");
 }
-const inviteBtn = document.querySelectorAll(".invite-btn");
+
 inviteBtn.forEach(btn => btn.addEventListener("click", modalFunction));
 
 modalClose.addEventListener("click", function() {
